f6e142a17446696d40eb48ca540c710ab201cf33
Durgesh <durgeshmehar1380@gmail.com>
2022-12-13 20:22:00 +0530

    firstcommit

---
+++ Portfolio Page/demo.js
@@ -0,0 +1,9 @@
+ let element
+
+ element = document.querySelectorAll("li")[1]
+
+element.style.color="green"
+
+console.log(element);
+
+//https://meet.google.com/fpx-wchz-nxu
--- Portfolio Page/main.js
+++
@@ -1,8 +0,0 @@
-let element 
-
-//document.getElementById("header").innerHTML= "<h1>Hello</h1>";
-
-
-element = document.getElementById("header").innerHTML = "<h1>Ram</h1>"
-
-console.log(element)
