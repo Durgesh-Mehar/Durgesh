ba24c6628b6a7adc0a5a6b740b13ba0d485a03fc
Durgesh-Mehar <120379146+Durgesh-Mehar@users.noreply.github.com>
2022-12-14 21:32:29 +0530

    Add files via upload

---
+++ demo.js
@@ -0,0 +1,78 @@
+//Creating Nodes and Modifying Dom :-
+
+//let ItemList = document.querySelector("#items")
+//console.log(ItemList.parentNode);
+//ItemList.parentNode.style.backgroundColor = "#f4f4f4"
+//console.log(ItemList.parentNode.parentNode);
+
+//parent Element
+//console.log(ItemList.parentElement);
+//ItemList.parentElement.style.backgroundColor = "#f4f4f4"
+//console.log(ItemList.parentElement.parentElement);
+
+//child Nodees
+//console.log(ItemList.childNodes)
+
+//console.log(ItemList.children);
+//console.log(ItemList.children[1]);
+//ItemList.children[1].style.backgroundColor = "yellow"
+
+//Frist Child
+//console.log(ItemList.firstChild);
+
+//Frist Element Child
+//console.log(ItemList.firstElementChild)
+//ItemList.firstElementChild.textContent ="Hello World"
+
+//Last Child
+//console.log(ItemList.lastChild);
+
+//Last Element Child
+//console.log(ItemList.lastElementChild)
+//ItemList.lastElementChild.textContent = "Hello World"
+
+//Next Sibling
+//console.log(ItemList.nextSibling);
+
+//Next Element Sibling
+//console.log(ItemList.nextElementSibling);
+   
+//Previous Sibling
+//console.log(ItemList.previousSibling);
+// Previuos Element Sibling
+//console.log(ItemList.previousElementSibling)
+//ItemList.previousElementSibling.style.color = "green"
+
+//Create Element
+
+// Create div
+////let newDiv= document.createElement("div")
+
+// Create ClassName
+//newDiv.className="Hello"
+
+// Create id 
+//newDiv.id = "hello 1"
+
+//Create Attribute
+//newDiv.setAttribute('title','Hello 3')
+
+//Create text Node
+//newDivText = document.createTextNode("HEllo")
+
+// Add text to div
+//newDiv.appendChild(newDivText)
+
+//let container = document.querySelector("header .container")
+//let h1 = document.querySelector("header h1")
+
+//console.log(newDiv)
+//container.insertBefore(newDiv,h1)
+
+
+//let container = document.querySelector("ul")
+//let h1 = document.querySelector("ul li")
+
+//console.log(newDiv)
+//container.insertBefore(newDiv,h1)
+
