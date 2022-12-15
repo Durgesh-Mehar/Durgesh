cfbe6d8f3ea23d9a2699c1c93f60a71a75044d4d
Durgesh-Mehar <120379146+Durgesh-Mehar@users.noreply.github.com>
2022-12-15 16:54:02 +0530

    How to use filter in DOM

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
 var form = document.getElementById('addForm');
 var itemList = document.getElementById('items');
+var filter = document.getElementById("filter")
 form.addEventListener('submit',addItem);
 itemList.addEventListener('click',removeItem); 
+//Filter Item
+filter.addEventListener("keyup",filterItems);
 function addItem(e){
     e.preventDefault();
     var newItem = document.getElementById('item').Value;
@@ -21,4 +24,21 @@ function removeItem(e){
             itemList.removeChild(li);
         }
     }
+}
+
+//Filter Items 
+function filterItems(e){
+   // convert text to lowercase
+   var text = e.target.value.toLowerCase();
+   //Get lis
+   var items = itemsList .getElementsByTagName("li");
+   //Convert to an Array
+   Array.from(items).forEach(function(item){
+      var itemName = item.fristChild.textContent;
+      if(itemName.toLowerCase().indexOf(text) != -1){
+         items.style.display = "block"
+      }else{
+         item.style.display = "none"
+      }
+   })
 }
