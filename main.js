d0f2f61d58315285ccefd54eac3f86b8026d2792
Durgesh-Mehar <120379146+Durgesh-Mehar@users.noreply.github.com>
2022-12-16 11:17:17 +0530

    How to use LocalStorage & SessionStorage

--- main.js
+++ main.js
@@ -1,44 +1,16 @@
-var form = document.getElementById('addForm');
-var itemList = document.getElementById('items');
-var filter = document.getElementById("filter")
-form.addEventListener('submit',addItem);
-itemList.addEventListener('click',removeItem); 
-//Filter Item
-filter.addEventListener("keyup",filterItems);
-function addItem(e){
-    e.preventDefault();
-    var newItem = document.getElementById('item').Value;
-    var li = document.createElement('li');
-    li.className = 'list-group-item';
-    li.appendChild(document.createTextNode(newItem));
-    var deleteBtn = document.createElement('button');
-    deleteBtn.className = 'btn btn-danger btn-sm float-right delete';
-    deleteBtn.appendChild(document.createTextNode('X'));
-    li.appendChild(deleteBtn);
-    itemList.appendChild(li);
-}
-function removeItem(e){
-    if(e.target.classList.contains('delete')){
-        if(confirm('Are You Sure?')){
-            var li = e.target.parentElement;
-            itemList.removeChild(li);
-        }
-    }
-}
+// Local Storage
+localStorage.setItem("name","Rohan");
+localStorage.removeItem("name");
 
-//Filter Items 
-function filterItems(e){
-   // convert text to lowercase
-   var text = e.target.value.toLowerCase();
-   //Get lis
-   var items = itemsList .getElementsByTagName("li");
-   //Convert to an Array
-   Array.from(items).forEach(function(item){
-      var itemName = item.fristChild.textContent;
-      if(itemName.toLowerCase().indexOf(text) != -1){
-         items.style.display = "block"
-      }else{
-         item.style.display = "none"
-      }
-   })
-}
+//session Storage
+sessionStorage.setItem("name", "Dilip")
+sessionStorage.setItem("name", "Ravi")
+sessionStorage.setItem("Name", "Ram")
+sessionStorage.removeItem("name")
+
+// Cookie 
+
+document.cookie = "name = Ram; expires=" + new Date(9999,0,1).toUTCString();
+document.cookie = "name = Dilip; expires=" + new Date(9999,0,1).toUTCString();
+
+console.log(document.cookie)
