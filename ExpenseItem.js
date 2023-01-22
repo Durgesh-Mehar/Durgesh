18b163ab81ce70b9d2bc481cc616835ca567e3fc
Durgesh-Mehar <120379146+Durgesh-Mehar@users.noreply.github.com>
2023-01-21 23:26:40 -0800

    Just hard code for now

---
+++ ExpenseItem.js
@@ -0,0 +1,18 @@
+ function ExpenseItem(){
+    return (
+        <div>
+            <h2>Expense Items</h2>
+            <div>
+             <h2>Food </h2>
+             <div>Rs 10</div>
+             <h2>Petrol</h2>
+             <div>Rs 100</div>
+             <h2>Movies</h2>
+             <div>Rs 200</div>
+            </div>
+        </div>
+    )
+    
+ }
+
+ export default ExpenseItem;
